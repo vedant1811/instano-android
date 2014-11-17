5375ff5e1fccf13ca3ad939b13974d77c3fd59ce
Vedant Agarwala <vedant.kota@gmail.com>
2014-11-17 22:26:40 +0530

    added a my location button to maps activity

--- app/src/main/java/com/instano/retailer/instano/activities/MapsActivity.java
+++ app/src/main/java/com/instano/retailer/instano/activities/MapsActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.instano.retailer.instano.R;
 
 public class MapsActivity extends Activity {
@@ -61,6 +59,7 @@ public class MapsActivity extends Activity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
 }
