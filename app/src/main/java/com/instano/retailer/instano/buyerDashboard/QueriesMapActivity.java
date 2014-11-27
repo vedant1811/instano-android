b33506e52d02612d45f2f673e0857ba77d3ebe5f
Vedant Agarwala <vedant.kota@gmail.com>
2014-11-27 18:43:30 +0530

    new file:   app/src/main/java/com/instano/retailer/instano/buyerDashboard/QueriesMapActivity.java Just a barebones map activity to be populated later

---
+++ app/src/main/java/com/instano/retailer/instano/buyerDashboard/QueriesMapActivity.java
@@ -0,0 +1,84 @@
+package com.instano.retailer.instano.buyerDashboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.instano.retailer.instano.R;
+
+import java.util.List;
+
+public class QueriesMapActivity extends Activity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queries_map);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Use to create a bundle that is easily (and reliably) parsed by this activity when it is created.
+     * @param queryIds The list of queries to be displayed. The search string, location,
+     */
+    public static Bundle createBundle(List<Integer> queryIds) {
+        return null;
+    }
+
+    /**
+     * Use to create a bundle that is easily (and reliably) parsed by this activity when it is created.
+     *
+     */
+    public static Bundle createBundle(List<Integer> sellerIds, String search, LatLng position, int radius) {
+        return null;
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link MapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
