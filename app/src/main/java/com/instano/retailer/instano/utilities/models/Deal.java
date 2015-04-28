359d60268b6eb5658b4f9e86949fca5c36926856
Rohit Roy <rohit7roy@gmail.com>
2015-04-28 15:33:38 +0530

    Offer List created and working

--- app/src/main/java/com/instano/retailer/instano/utilities/models/Deal.java
+++ app/src/main/java/com/instano/retailer/instano/utilities/models/Deal.java
@@ -1,5 +1,6 @@
 package com.instano.retailer.instano.utilities.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.instano.retailer.instano.application.ServicesSingleton;
 
 import java.util.Date;
@@ -8,12 +9,20 @@ import java.util.Date;
  * Created by vedant on 7/1/15.
  */
 public class Deal {
+    @JsonProperty("id")
     public int id;
+    @JsonProperty("heading")
     public String heading;
+    @JsonProperty("subheading")
     public String subheading;
-    public long updatedAt;
+    @JsonProperty("updated_at")
+    public Date updatedAt;
+    @JsonProperty("expires_at")
     public Date expiresAt;
+    @JsonProperty("seller_id")
     public int sellerId;
+    @JsonProperty("product")
+    public Product product;
 
     public String expiresAt() {
         return "expires " + ServicesSingleton.instance().getPrettyTimeElapsed(expiresAt);
