32b6e6c5ee1a85e0a912af74124ff6de9d323d6d
Rohit Roy <rohit7roy@gmail.com>
2015-04-29 18:04:51 +0530

    working fb login

--- app/src/main/java/com/instano/retailer/instano/activities/ProfileActivity.java
+++ app/src/main/java/com/instano/retailer/instano/activities/ProfileActivity.java
@@ -1,19 +1,32 @@
 package com.instano.retailer.instano.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ViewFlipper;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 import com.instano.retailer.instano.R;
+import com.instano.retailer.instano.application.BaseActivity;
 import com.instano.retailer.instano.application.ServicesSingleton;
 import com.instano.retailer.instano.application.network.NetworkRequestsManager;
 import com.instano.retailer.instano.application.network.ResponseError;
 import com.instano.retailer.instano.utilities.library.Log;
 import com.instano.retailer.instano.utilities.models.Buyer;
 
-public class ProfileActivity extends GlobalMenuActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProfileActivity extends BaseActivity {
 
     private static final String ALREADY_TAKEN_ERROR = "already taken. Contact us if this is an error";
     private static final String TAG ="ProfileActivity";
@@ -25,6 +38,24 @@ public class ProfileActivity extends GlobalMenuActivity {
     CharSequence mName;
     CharSequence mPhone;
     int mViewFlipperState;
+    Session facebookSession;
+    List<String> mPermissions = new ArrayList<String>();
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+       public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+         }
+    };
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.v(TAG, "Logged in...");
+
+        } else if (state.isClosed()) {
+            Log.v(TAG, "Logged out...");
+        }
+    }
 
     @Override
     protected void onPause() {
@@ -52,6 +83,14 @@ public class ProfileActivity extends GlobalMenuActivity {
         mPhoneEditText = (EditText) findViewById(R.id.phoneEditText);
         mSetUpViewFlipper = (ViewFlipper) findViewById(R.id.setUpViewFlipper);
         mSetUpButton = (Button) findViewById(R.id.setUpButton);
+        LoginButton facebookLogin = (LoginButton) findViewById(R.id.fbLogin);
+
+        mPermissions.add("public_profile");
+        mPermissions.add("user_location");
+        mPermissions.add("email");
+        mPermissions.add("user_birthday");
+        mPermissions.add("user_friends");
+        facebookLogin.setReadPermissions(mPermissions);
 
         // TODO:
 //        mPhoneEditText.setOnFocusChangeListener((v, hasFocus) -> {
@@ -62,6 +101,26 @@ public class ProfileActivity extends GlobalMenuActivity {
 //            }
 //        });
 
+        facebookLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+           public void onClick(View view) {
+                if(Session.getActiveSession() == null )
+                    openActiveSession(ProfileActivity.this, true, mPermissions,callback);
+                else {
+                    Request.newMeRequest(Session.getActiveSession(), new Request.GraphUserCallback() {
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            Log.v(TAG, "Birthday: "+user.getBirthday());
+                            Log.v(TAG, "UserId: "+user.getId());
+                            Log.v(TAG, "User birthday : " + user.getName());
+                            Log.v(TAG,"Response : "+response);
+
+                        }
+                    }).executeAsync();
+                }
+           }
+       });
+
         // check if a user exists:
         Buyer buyer = ServicesSingleton.instance().getBuyer();
         if (buyer != null) {
@@ -71,6 +130,34 @@ public class ProfileActivity extends GlobalMenuActivity {
 //            mSetUpButton.setText("Update");
         }
     }
+    private void openFacebookSession(){
+                String[] list={"user_likes", "user_status","name","user_birthday","email","public_profile"};
+                Session.openActiveSession(this, true, Arrays.asList(list), new Session.StatusCallback() {
+                    @Override
+                    public void call(Session session, SessionState state, Exception exception) {
+                        if (state.isOpened()) {
+                            ServicesSingleton.instance().facebookSession = session;
+                            Log.v(TAG, "Logged in...");
+                        }
+                        else if (state.isClosed()) {
+                             Log.v(TAG, "Logged out...");
+                        }
+                         // you can make request to the /me API or do other stuff like post, etc. here
+                     }
+                 });
+            }
+
+    private static Session openActiveSession(Activity activity, boolean allowLoginUI, List permissions, Session.StatusCallback callback) {
+           Session.OpenRequest openRequest = new Session.OpenRequest(activity).setPermissions(permissions).setCallback(callback);
+           Session session = new Session.Builder(activity).build();
+          if (SessionState.CREATED_TOKEN_LOADED.equals(session.getState()) || allowLoginUI) {
+                Session.setActiveSession(session);
+            session.openForRead(openRequest);
+                return session;
+         }
+            return null;
+        }
+
 
     private boolean checkPhoneNumber() {
         String text = mPhoneEditText.getText().toString();
@@ -139,4 +226,11 @@ public class ProfileActivity extends GlobalMenuActivity {
         setResult(RESULT_OK);
         finish();
     }
+    @Override
+   public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
+
 }
