bf1eb8dc3fb95896b70352f9f1e884e94ba23a28
Dheeraj <dheerajmeena0709@gmail.com>
2015-05-07 13:44:04 +0530

    Added layout for store

--- app/src/main/java/com/instano/retailer/instano/activities/home/HomeActivity.java
+++ app/src/main/java/com/instano/retailer/instano/activities/home/HomeActivity.java
@@ -27,7 +27,7 @@ public class HomeActivity extends BaseActivity
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     * Used to StoreActivity the last screen title. For use in {@link #restoreActionBar()}.
      */
     private CharSequence mTitle;
     private boolean mTwoPane;
